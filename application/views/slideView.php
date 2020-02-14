b2d4ce9d703d48999132d3d529cf4355daeb63ad
gk <jebbarirayhane@gmail.com>
2020-02-14 17:14:24 +0100

    slide

--- application/views/slideView.php
+++ application/views/slideView.php
@@ -1,149 +1,27 @@
 
 <html lang="en">
 	<head>
-        <style>
-            
-            @media only screen and (orientation: portrait) {
-  #the-canvas{
-      height: 50%!important;
-  }
-}
-            </style>
+    <link rel = "stylesheet" href = "<?php echo base_url()?>css/slideView.css"/>
+    <link href="<?php echo base_url();?>assets/fontawesome/css/all.css" rel="stylesheet">
     <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
+    <script>let base_url = "<?php echo base_url();?>";</script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div style = "position:fixed; margin-top:50px;">
-  <button id="prev">Previous</button>
-  <button id="next">Next</button>
-  &nbsp; &nbsp;
-  <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span>
-</div> 
-<button id="fullscreen" style ="margin-top:100px;">fullscreen</button>
+  </head>
+<body>
 
-<canvas id="the-canvas" style = "height:100%; width:100%;"></canvas>
-<script>
-    var url = "<?php echo base_url();?>" + "uploads/images/m.pdf";
+  <!-- <div id = "count"><span>Page: <span id="page_num"></span> / <span id="page_count"></span></span></div> -->
 
 
-// Loaded via <script> tag, create shortcut to access PDF.js exports.
-var pdfjsLib = window['pdfjs-dist/build/pdf'];
+<div class = "container">
+  <canvas id="the-canvas" style = "height:100%; width:100%; ">
+  </canvas>
+  <div class="fas fa-arrow-circle-right" id = "next"></div>
+  <div class="fas fa-arrow-circle-left" id ="prev"></div>
+  <i class="fas fa-compress" id="fullscreen"></i>
+</div>
 
-// The workerSrc property shall be specified.
-pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+<script src = "<?php echo base_url()?>js/slideView.js"></script>
 
-var pdfDoc = null,
-    pageNum = 1,
-    pageRendering = false,
-    pageNumPending = null,
-    canvas = document.getElementById('the-canvas'),
-    scale = 2.0,
-    ctx = canvas.getContext('2d');
-
-/**
- * Get page info from document, resize canvas accordingly, and render page.
- * @param num Page number.
- */
-function renderPage(num) {
-  pageRendering = true;
-  // Using promise to fetch the page
-  pdfDoc.getPage(num).then(function(page) {
-    var viewport = page.getViewport({scale : scale});
-    console.log(viewport);
-    console.log(window.innerWidth/window.innerHeight);
-    console.log( window.innerWidth/ viewport.width);
-    scale = window.innerWidth/0.2/ (viewport.width);
-    console.log(scale)
-    viewport = page.getViewport({scale : scale});
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    // Render PDF page into canvas context
-    var renderContext = {
-      canvasContext: ctx,
-      viewport: viewport
-    };
-    var renderTask = page.render(renderContext);
-
-    // Wait for rendering to finish
-    renderTask.promise.then(function() {
-      pageRendering = false;
-      if (pageNumPending !== null) {
-        // New page rendering is pending
-        renderPage(pageNumPending);
-        pageNumPending = null;
-      }
-    });
-  });
-
-  // Update page counters
-  document.getElementById('page_num').textContent = num;
-}
-
-/**
- * If another page rendering in progress, waits until the rendering is
- * finised. Otherwise, executes rendering immediately.
- */
-function queueRenderPage(num) {
-  if (pageRendering) {
-    pageNumPending = num;
-  } else {
-    renderPage(num);
-  }
-}
-
-/**
- * Displays previous page.
- */
-function onPrevPage() {
-  if (pageNum <= 1) {
-    return;
-  }
-  pageNum--;
-  queueRenderPage(pageNum);
-}
-document.getElementById('prev').addEventListener('click', onPrevPage);
-
-/**
- * Displays next page.
- */
-function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
-    return;
-  }
-  pageNum++;
-  queueRenderPage(pageNum);
-}
-document.getElementById('next').addEventListener('click', onNextPage);
-
-/**
- * Asynchronously downloads PDF.
- */
-pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
-  pdfDoc = pdfDoc_;
-  document.getElementById('page_count').textContent = pdfDoc.numPages;
-
-  // Initial/first page rendering
-  renderPage(pageNum);
-});
-
-button = document.getElementById('fullscreen');
-canvas = document.getElementById('the-canvas');
-button.addEventListener('click', function(){
-    if (button.requestFullscreen) {
-    canvas.requestFullscreen();
-    }
-});
-
-// window.addEventListener("orientationchange", function(){
-//     if(window.screen.orientation.type === "landscape-primary"){
-//         canvas
-//     }
-//     else if (window.screen.orientation.type === "portrait-primary"){
-//         canvas = document.getElementById('the-canvas');
-//         canvas.height.height*2;
-        
-//     }
-// })
-
-    </script>
 	</body>
 </html>
